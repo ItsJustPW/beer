baff28099b13ff54c7cbd3e8aa8017bf37245ead
ItsJustPW <pwhittle@me.com>
2021-02-18 19:50:16 +0000

    Refactor and adding debugging

--- app.js
+++ app.js
@@ -1,36 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-const startBtn = document.querySelector('.beer-button')
-const randomBeer = document.querySelector('.random-beer')
-const descriptionDisplay = document.querySelector('.description')
-const abvDisplay = document.querySelector('.abv')
-const foodPairing = document.querySelector('.food')
+    const startBtn = document.querySelector('.beer-button')
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+    const abvDisplay = document.querySelector('.abv')
+    const foodPairing = document.querySelector('.food')
+    const debug = true
 
-function getData(e){
-    e.preventDefault()
-    
-    fetch('https://api.punkapi.com/v2/beers/random')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-    //    console.log(data)
-        const name = data[0].name
-        const description = data[0].description
-        const abv = data[0].abv
-        const food = data[0].food_pairing
-        console.log(food)
-        const {volume} = data[0]
-        const volumeValue = volume.value
-        const volumeUnit = volume.unit
-    //   console.log(volume.value, volume.unit)
-    
-        randomBeer.innerHTML = name // + ' ' + volumeValue + ' ' + volumeUnit
-        abvDisplay.innerHTML = abv + "%"
-        descriptionDisplay.innerHTML = description
-        foodPairing.innerHTML = food
-    })
-}
+    function log(data) {
+        if (debug) {
+            console.log(data)
+        }
+    }
 
-startBtn.addEventListener('click', getData)
+    function getData(e){
+        e.preventDefault()
+        
+        fetch('https://api.punkapi.com/v2/beers/random')
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            log(data)
+            const name = data[0].name
+            const description = data[0].description
+            const abv = data[0].abv
+            const food = data[0].food_pairing
+            log(food)
+            const {volume} = data[0]
+            const volumeValue = volume.value
+            const volumeUnit = volume.unit
+        
+            randomBeer.innerHTML = name // + ' ' + volumeValue + ' ' + volumeUnit
+            abvDisplay.innerHTML = abv + "%"
+            descriptionDisplay.innerHTML = description
+            foodPairing.innerHTML = food
+        })
+    }
+
+    startBtn.addEventListener('click', getData)
 
 })
