acc1382da264e6b1ac59ed26f1d154bc599a7daa
ItsJustPW <pwhittle@me.com>
2021-02-18 16:35:36 +0000

    Adding the food pairing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const startBtn = document.querySelector('.beer-button')
 const randomBeer = document.querySelector('.random-beer')
 const descriptionDisplay = document.querySelector('.description')
 const abvDisplay = document.querySelector('.abv')
+const foodPairing = document.querySelector('.food')
 
 function getData(e){
     e.preventDefault()
@@ -12,19 +13,21 @@ function getData(e){
         return response.json()
     })
     .then(data => {
-        console.log(data)
+    //    console.log(data)
         const name = data[0].name
         const description = data[0].description
         const abv = data[0].abv
-        console.log(abv)
+        const food = data[0].food_pairing
+        console.log(food)
         const {volume} = data[0]
         const volumeValue = volume.value
         const volumeUnit = volume.unit
-     //   console.log(volume.value, volume.unit)
+    //   console.log(volume.value, volume.unit)
     
         randomBeer.innerHTML = name // + ' ' + volumeValue + ' ' + volumeUnit
         abvDisplay.innerHTML = abv + "%"
         descriptionDisplay.innerHTML = description
+        foodPairing.innerHTML = food
     })
 }
 
